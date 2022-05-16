3806a760fd1987ce93b9076ead3be5b6eea61298
Facundo Sad <Sadfacundo@gmail.com>
2022-05-16 10:44:41 -0300

    Calculadora con REACT. Primer commit

---
+++ src/componentes/BotonClear.js
@@ -0,0 +1,14 @@
+import '../hojas-de-estilo/BotonClear.css'
+import React from 'react';
+
+
+const BotonClear = ( props ) => (
+<div className='boton-clear'
+      onClick={props.manejarClear}>
+    {props.children}
+</div>
+);
+
+
+
+export default BotonClear;
---
+++ src/componentes/Pantalla.js
@@ -0,0 +1,14 @@
+import '../hojas-de-estilo/Pantalla.css';
+import React from 'react';
+
+
+const Pantalla = ( { input } ) => (
+
+    <div className='input'>
+        {input}
+    </div>
+
+);
+
+
+export default Pantalla;
